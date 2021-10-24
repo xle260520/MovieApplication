2257923a6d9cd4d9446ac398c65dc2d18701af87
xle260520 <91984671+xle260520@users.noreply.github.com>
2021-10-24 15:37:29 -0400

    Add files via upload

---
+++ Back-end/MovieApplication.Data/Models/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MovieApplication.Data.Models
+{
+    public partial class Genre
+    {
+        public Genre()
+        {
+            TitleGenres = new HashSet<TitleGenre>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<TitleGenre> TitleGenres { get; set; }
+    }
+}
