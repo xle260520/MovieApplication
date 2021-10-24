e86e1361f459ec4acf749a98aa065aa5ae377dde
xle260520 <91984671+xle260520@users.noreply.github.com>
2021-10-24 15:35:50 -0400

    Add files via upload

---
+++ Back-end/MovieApplication/Controllers/TitlesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApplication.Service;
+
+
+namespace Crossroad.Controllers
+{
+    [Route("api/titles")]
+    [ApiController]
+    public class TitlesController : ControllerBase
+    {
+
+        private readonly ITitleService _titleService;
+
+        public TitlesController(ITitleService titleService)
+        {
+            _titleService = titleService;
+        }
+       
+      
+
+        [HttpGet]
+        public  IActionResult GetAll()
+        {
+            var title = _titleService.getAllTitles();
+            return Ok(title);
+        }
+
+        
+        
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var title = _titleService.getAllTitlesByName(name);
+            return Ok(title);
+        }
+        
+    }
+}
