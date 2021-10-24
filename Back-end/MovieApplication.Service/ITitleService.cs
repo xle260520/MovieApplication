0f69f1446ef00baad47c82e43a7ea035fec10e6e
xle260520 <91984671+xle260520@users.noreply.github.com>
2021-10-24 15:38:31 -0400

    Add files via upload

---
+++ Back-end/MovieApplication.Service/ITitleService.cs
@@ -0,0 +1,15 @@
+﻿using MovieApplication.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApplication.Service
+{
+    public interface ITitleService
+    {
+        List<Title> getAllTitles();
+        List<Title> getAllTitlesByName(string name);
+    }
+}
